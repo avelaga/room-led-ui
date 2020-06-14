f15865f98590e2aa8efd762924297485ba82f4d4
Abhi Velaga <abhinav.velaga@gmail.com>
2020-06-14 01:24:53 -0500

    removed all for now

---
+++ src/components/Section.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Slider from '@material-ui/core/Slider';
+import './components.css';
+const axios = require("axios").default;
+
+export class Section extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick(num) {
+    this.props.urls.map((value, index) => {
+      let url = value + "/hue?hue=" + num;
+      axios
+        .get(
+          url
+        )
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+  }
+
+  onChangeCommitted = (event, newValue) => {
+    this.handleClick(newValue)
+  };
+
+  render() {
+    return (
+      <div className="flex">
+        <div onClick={() => this.handleClick(0)} className="button" > Runway </div>
+        <Slider
+          // value={0}
+          onChange={this.onChangeCommitted}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          style={sliderStyle}
+          min={1}
+          max={255}
+        />
+      </div>
+    )
+  }
+}
+
+export default Section
+
+const sliderStyle = {
+  width: '200px',
+  margin: '10px'
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,17 @@
 import React, { Component } from 'react'
-import Slider from '@material-ui/core/Slider';
+import Section from '../components/Section.js';
 import './pages.css';
-const axios = require("axios").default;
 
 export class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // color1: 
-      hue: 0
-    };
-  }
-
-  handleClick(num) {
-    let url = "http://micropenis1.local:8000/hue?hue=" + num;
-    axios
-      .get(
-        url
-      )
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    // console.log(num)
-  }
-
-  onChangeCommitted = (event, newValue) => {
-    this.handleClick(newValue)
-  };
-
   render() {
     return (
-      <div className="flex">
-        <div onClick={() => this.handleClick(0)} className="button" > Runway </div>
-        <Slider
-        // value={0}
-          onChange={this.onChangeCommitted}
-          valueLabelDisplay="auto"
-          aria-labelledby="range-slider"
-          style={sliderStyle}
-          min={1}
-          max={255}
-        />
+      <div className="container">
+        {/* <h1>All</h1>
+        <Section urls={["http://micropenis1.local:8000"],["http://micropenis2.local:8000"]}/> */}
+        <h1>Wall</h1>
+        <Section urls={["http://micropenis1.local:8000"]}/>
+        <h1>Closet</h1>
+        <Section urls={["http://micropenis2.local:8000"]}/>
       </div>
     )
   }
